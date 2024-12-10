940b9fea8bb5bddc4fdb37c947a7dcd07253c6e0
RishanaS <139629595+RishanaS@users.noreply.github.com>
2024-12-10 13:21:43 +0000

    signup.php created

--- php/signup.php
+++ php/signup.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
 
 	require_once 'dbconf.php';	
@@ -29,12 +21,4 @@ function AddSignupDetails($connect,$name,$email,$user_name,$password){
 			    die($e->getMessage());
 	        }
 		}
-
-    
-
-    ?>
-
-
-    </body>
-    </html>
-    
+?>   
