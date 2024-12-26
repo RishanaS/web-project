441a0f83a620365dab041d4e255246c21bd766db
RishanaS <139629595+RishanaS@users.noreply.github.com>
2024-12-26 15:32:24 +0000

    index file created

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Dressify</title>
+</head>
+<body>
+    <h1>Dressify</h1><br><br>
+
+    <a href="frontpage.html">Front Page</a><br>
+
+
+    
+</body>
+</html>
