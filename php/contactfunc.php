3426b58fe0f29eb091252cd23c4d47c93a8c78b5
RishanaS <139629595+RishanaS@users.noreply.github.com>
2024-12-26 14:52:55 +0000

    contactfunc.php created

---
+++ php/contactfunc.php
@@ -0,0 +1,28 @@
+<?php
+	require_once 'dconf.php';
+	function AddData($connect,$User_Name,$Email_Address,$Subject,$Comment){
+		try {
+        $sql = "INSERT INTO ContactUs VALUES('$User_Name','$Email_Address','$Subject','$Comment')";
+			
+			$result = mysqli_query($connect,$sql);
+			if ($result) {
+				  echo "create Account sucessfully";
+			} else {
+				die("Error ".mysqli_error($connect));
+			}
+            exit;
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+	
+	if ($_SERVER['REQUEST_METHOD'] == "POST") {
+		$User_Name = $_POST['username'];
+		$Email_Address = $_POST['email'];
+        $Subject = $_POST['subject'];
+		$Comment = $_POST['message'];
+		AddData($connect,$User_Name,$Email_Address,$Subject,$Comment);
+	}
+	
+
+	?>
